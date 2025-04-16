438103c24359e888f2f02278b4d43201066d3343
Ahlem cherni <ahlem.cherni112@gmail.com>
2025-04-17 01:23:00 +0200

    fix micoservices

---
+++ erp-config/src/test/java/com/iss4u/erp/services/ERPConfigApplicationTests.java
@@ -0,0 +1,15 @@
+package com.iss4u.erp.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERPConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
