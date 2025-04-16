52383fb6a0e7ead2c54839aa4be89e292c28236c
ahlem25 <ahlem.cherni112@gmail.com>
2025-04-16 16:22:41 +0100

    first commit

---
+++ erp-user-service/src/main/java/com/iss4u/erp/services/ERPUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.iss4u.erp.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERPUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERPUserServiceApplication.class, args);
+	}
+
+}
