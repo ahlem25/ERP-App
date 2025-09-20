d207bc8f9869030db10ce69fd8169006f4ab1644
Ahlem cherni <ahlem.cherni112@gmail.com>
2025-09-20 19:52:13 +0200

    update dashboard url

--- erp-dashboard-service/src/main/java/com/iss4u/erp/services/controller/DashboardController.java
+++ erp-dashboard-service/src/main/java/com/iss4u/erp/services/controller/DashboardController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/dashboard")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class DashboardController {
 
