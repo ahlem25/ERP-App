a61324077cc73b0da87998cad84cf9ad42ba96ab
Ahlem Cherni <ahlem.cherni112@gmail.com>
2025-10-18 20:49:46 +0200

    update iac

--- .iac/ecr.tf
+++ .iac/ecr.tf
@@ -31,6 +31,7 @@ resource "aws_ecr_repository" "erp_services" {
 
   name                 = each.key
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
